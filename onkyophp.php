a5c35f40a58b4de16ae21d6010f15fe12018c6b4
jillesv <jilles@jillesathome.nl>
2018-10-22 13:44:57 +0200

    Add files via upload

---
+++ onkyophp.php
@@ -0,0 +1,63 @@
+<?php
+
+    $command = $_GET['command'];
+
+    $params = $_GET['params'];
+
+ 
+
+    $hostname = '172.19.3.71';
+
+    $port = 60128;
+
+ 
+
+    switch ($command)
+
+    {
+
+        case 'PWR':
+
+        case 'AMT':
+
+        case 'SLI':
+
+        case 'TUN':
+	
+	case 'MVLUP':
+
+	case 'MVLDOWN':
+
+            $message = '!1' . $command . $params;
+
+            break;
+
+        case 'MVL':
+
+            if ($params > 60)
+
+                exit(1);
+
+            $message = '!1' . $command . strtoupper(str_pad(dechex($params), 2, '0', STR_PAD_LEFT));
+
+            break;
+
+        default:
+
+            exit(1);
+
+    }
+
+    print $message;
+
+ 
+
+    $fp = pfsockopen($hostname, $port);
+
+    $packet = "ISCP\x00\x00\x00\x10\x00\x00\x00" . chr(strlen($message) + 1) . "\x01\x00\x00\x00" . $message . "\x0D";
+
+    fwrite($fp, $packet);
+
+    fclose($fp);
+
+?>
